4f9f9b9a5f09ab13bc14674f129c17aa7cf6ebb0
Joshua Davenport <davenport.e.joshua@gmail.com>
2024-04-25 17:29:01 -0500

    Nvim UNDONE ALL CHANGES AAAAH

--- user/apps/neovim.nix
+++ user/apps/neovim.nix
@@ -11,13 +11,6 @@
       neo-tree-nvim
     ];
     extraConfig = ''
---[[ init.lua ]]
-
--- LEADER
--- These keybindings need to be defined before the first /
--- is called; otherwise, it will default to "\"
-vim.g.mapleader = " "
-vim.g.localleader = "\\"
     '';
   };
 
