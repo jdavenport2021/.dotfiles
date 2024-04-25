c53e169d60b940b9c23428b1b69f1918ab1751b7
Joshua Davenport <davenport.e.joshua@gmail.com>
2024-04-25 17:27:46 -0500

    Nvim Config Change 6

--- user/apps/neovim.nix
+++ user/apps/neovim.nix
@@ -11,7 +11,13 @@
       neo-tree-nvim
     ];
     extraConfig = ''
-map('i', 'jk', '<Esc>', {})
+--[[ init.lua ]]
+
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = " "
+vim.g.localleader = "\\"
     '';
   };
 
