44beb07b560afbcce45a57b8683019d17d98a73b
Joshua Davenport <davenport.e.joshua@gmail.com>
2024-04-25 21:10:08 -0500

    Nvim configLUA

--- user/apps/neovim.nix
+++ user/apps/neovim.nix
@@ -7,10 +7,25 @@
 
   programs.neovim = {
     enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
     plugins = with pkgs.vimPlugins; [
       neo-tree-nvim
     ];
-    extraConfig = ''
+    extraLuaConfig = ''
+--[[ init.lua ]]
+
+-- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = " "
+vim.g.localleader = "\\"
+
+local map = vim.api.nvim_set_keymap
+
+-- remap the key used to leave insert mode
+map('i', 'jk', '<Esc>', {})
     '';
   };
 
